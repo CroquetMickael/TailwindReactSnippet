fb13f1a68d093971673379799d4d6995f922623f
Mickael CROQUET <mickael.croquet@axa.fr>
2020-08-03 16:23:33 +0200

    refactor(table) : Refactor table top

--- src/Component/Tables/TableTop.component.jsx
+++ src/Component/Tables/TableTop.component.jsx
@@ -1,19 +1,10 @@
 import React from "react";
+import { ItemsNumberSelect } from "./TableTop/ItemsNumberSelect.component";
 
-const TableTop = (props) => {
-  return props.NumberItemsOptions.permitChange !== false &&
-    props.paginated !== false ? (
-    <>
-      <select
-        className="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border border-gray-400 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-        onChange={(e) => props.setItemsPerPage(e.target.value)}
-      >
-        {props.NumberItemsOptions.options.map((option, index) => (
-          <option key={index}>{option}</option>
-        ))}
-      </select>
-    </>
-  ) : null;
-};
+const TableTop = (props) => (
+  <>
+    <ItemsNumberSelect {...props} />
+  </>
+);
 
 export { TableTop };
