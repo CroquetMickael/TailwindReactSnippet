0e5337484b9d881a3e63a663ecfa000a5a21ea82
unknown <mickael.croquet@laposte.net>
2020-09-09 11:06:37 +0200

    ui(autocomplete): UI improvement of input element

--- src/TestingComponentComposition/Input/Input.tsx
+++ src/TestingComponentComposition/Input/Input.tsx
@@ -11,20 +11,29 @@ interface InputProps {
 
 const Input = (props: InputProps) => (
   <>
-    <input
-      className="flex-1 block px-6 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l rounded-r appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
-      value={props.value}
-      placeholder={props.placeHolder}
-      onChange={props.onChange}
-    />
     {props.autocomplete ? (
-      <button
-        className="block px-6 py-2 text-sm bg-red-300 border border-b border-gray-400 rounded-r appearance-none hover:bg-red-500 sm:rounded-l-none"
-        onClick={props.onClick}
-      >
-        X
-      </button>
-    ) : null}
+      <>
+        <input
+          className="flex-1 block px-6 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+          value={props.value}
+          placeholder={props.placeHolder}
+          onChange={props.onChange}
+        />
+        <button
+          className="block px-6 py-2 text-sm bg-red-300 border border-b border-gray-400 rounded-r appearance-none hover:bg-red-500 sm:rounded-l-none"
+          onClick={props.onClick}
+        >
+          X
+        </button>
+      </>
+    ) : (
+      <input
+        className="flex-1 block px-6 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l rounded-r appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+        value={props.value}
+        placeholder={props.placeHolder}
+        onChange={props.onChange}
+      />
+    )}
   </>
 );
 
